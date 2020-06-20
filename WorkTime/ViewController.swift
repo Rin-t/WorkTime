f6ff6e254d64587bb26a46f6566dafec74ca8891
Rin <rin.t.4921@gmail.com>
2020-06-20 10:36:39 +0900

    Initial Commit

---
+++ WorkTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkTime
+//
+//  Created by Rin on 2020/06/20.
+//  Copyright © 2020 竹村凜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
